99d7390b4d49ed7b054ceeef297b563c4821b274
tyleroni <tjshin@ucsd.edu>
2024-01-15 02:19:07 -0800

    First half done

--- tyler/src/Components/Home/index.js
+++ tyler/src/Components/Home/index.js
@@ -6,15 +6,17 @@ const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm Tyler Shin
+                <h1 className = "text-zone text-zone2">Hi, </ h1>
+                <br /> 
+                <h1 className= 'text-zone text-zone3'>I'm Tyler Shin,</h1>
                 <br />
-                Web Developer
+                <h1 className='text-zone text-zone4'>Web Developer
                 </h1>
                 <h2>Frontend Developer / UI/UX Designer / Product Designer</h2>
-                <Link to ='/contact' className = 'flat-button'>CONTACT ME</Link>
+ 
             </div>
         </div>
     );
 }
-
+<Link to ='/contact' className = 'flat-button'>CONTACT ME</Link>
 export default Home
--- tyler/src/Components/Layout/index.js
+++ tyler/src/Components/Layout/index.js
@@ -6,19 +6,21 @@ const Layout = () => {
     return (
     <div className="App">
         <Sidebar />
-        
+        <div className = 'page'>
+            
+            <Outlet />
+ 
+        </div>
     </div>
     )
 }
 
 export default Layout
 
-//<div className = 'page'>
-//            <span className = 'tags top-tags'>&lt;body&gt;</span>
-//            <Outlet />
+//            <span className = 'tags top-tags'>&lt;&gt;</span>
+//
 //            <span className='tags bottom-tags'>
-//            &lt;/body&gt;
+ //           &lt;&gt;
 //            <br />
-//            <span className ='bottom-tag-html'>&lt;/html&gt;</span>
+ //           <span className ='bottom-tag-html'>&lt;&gt;</span>
 //            </span>
-//        </div>
