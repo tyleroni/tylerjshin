c895304a422b45ae05facd6fca363002dcaa8482
tyleroni <tjshin@ucsd.edu>
2024-01-15 19:12:21 -0800

    almost done

--- tyler/src/Components/Home/index.js
+++ tyler/src/Components/Home/index.js
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 import './index.scss';
-//import { background } from '../src/assets/images/Portfolio_Background.png';
+import Logo from '../../assets/images/Windows.gif';
 
 const Home = () => {
     return (
             <div className="container home-page">
-                <div className="text-zone">
-                    <h1 className = "text-zone text-zone2">Hi, </ h1>
-                    <br /> 
-                    <h1 className= 'text-zone text-zone3'>I'm Tyler Shin,</h1>
-                    <br />
-                    <h1 className='text-zone text-zone4'>Web Developer
-                    </h1>                
-                    <h2>Frontend Developer / UI/UX Designer / Product Designer</h2>
-                    <Link to ='/contact' className = 'flat-button'>CONTACT ME</Link>
+                <div className = "backgroundclr">
+                    <div className = "windows-gif" >
+                        <img src = {Logo} alt = "Windows Icon Gif"></img>
+                        </ div>
+                        <div className="text-zone">
+                        <h1 className = "text-zone text-zone2">Hi, </ h1>
+                        <br /> 
+                        <h1 className= 'text-zone text-zone3'>I'm Tyler Shin,</h1>
+                        <br />
+                        <h1 className='text-zone text-zone4'>Web Developer
+                        </h1>                
+                        <h2>Frontend Developer / UI/UX Designer / Product Designer</h2>
+                        <Link to ='/contact' className = 'flat-button'>CONTACT ME</Link>
+                    </div>
                 </div>
             </div>
     );
--- tyler/src/Components/Sidebar/index.js
+++ tyler/src/Components/Sidebar/index.js
@@ -2,13 +2,26 @@ import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/Tyler_Shin_white.png'
 import IconL from '../../assets/images/linkedin.png'
-
+import closeIcon from '../../assets/images/Close_button.png'
+import expandIcon from '../../assets/images/Expand_button.png'
+import minimizeIcon from '../../assets/images/Minimize_button.png'
 
 const Sidebar = () => (
     <div className = 'nav-bar'>
         <Link className='logo' to = '/'>
             <img src={LogoS} alt="Tyler Shin" />
         </Link>
+        <div className='control-tab'>
+            <Link className='window-icon' to = '/'>
+                <img src={minimizeIcon} alt="Windows Minimize Icon" width = '2%' height = 'auto'/>
+            </Link>
+            <Link className='window-icon' to = '/'>
+                <img src={expandIcon} alt="Windows Expand Icon" width = '2.05%' height = 'auto'/>
+            </Link>
+            <Link className='window-icon close-icon' to = '/'>
+                <img src={closeIcon} alt="Windows Close Icon" width = '2%' height = 'auto'/>
+            </Link>
+        </ div>
         <nav className = 'nav-bar2'>
             <ul>
                 <li className='active'>
