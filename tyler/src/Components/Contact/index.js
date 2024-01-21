6468de3eb0b7e7bd652e1a562e1c6101477591b9
tyleroni <tjshin@ucsd.edu>
2024-01-20 17:56:18 -0800

    finished contact form

--- tyler/src/Components/Contact/index.js
+++ tyler/src/Components/Contact/index.js
@@ -1,8 +1,29 @@
 import './index.scss'
-
+import emailjs from '@emailjs/browser'
+import {useRef} from 'react'
 
 const Contact = () => {
+    const refForm = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
 
+        emailjs
+        .sendForm(
+            'service_o8jokzn',
+            'template_0rha1sq',
+            refForm.current,
+            '-co4TL8Smd_fQEc4N'
+        )
+        .then(
+            () => {
+                alert('Message Sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again!')
+            }
+        )
+    }
     return (
     <>
         <div className = "contact-layout">
@@ -16,19 +37,19 @@ const Contact = () => {
                             I am open to networking and interested to hear from anybody about job opportunities, ideas or even games. I look forward to having a chat with you! 
                         </p>
                         <div className = 'contact-form'>
-                            <form>
+                            <form ref= {refForm} onSubmit = {sendEmail}> 
                                 <ul>
                                     <li className = 'half text-area1'>
-                                        <input type = 'text' name= 'Name' placeholder = 'Name' required/>
+                                        <input type = 'text' name= 'from_name' placeholder = 'Name' required/>
                                     </li>
                                     <li className = 'half text-area1'>
-                                        <input type = 'email' name= 'Email' placeholder = 'Email' required/>
+                                        <input type = 'email' name= 'email' placeholder = 'Email' required/>
                                     </li>
                                     <li className = 'text-area1'>
-                                        <input type = 'text' name = 'Subject' placeholder = 'Subject' required/>
+                                        <input type = 'text' name = 'subject' placeholder = 'Subject' required/>
                                     </li>
                                     <li>
-                                        <textarea type = 'text' name = 'Message' placeholder = "Message" required/>
+                                        <textarea type = 'text' name = 'message' placeholder = "Message" required/>
                                     </li>
                                     <li>
                                         <input type = 'submit' className = 'flat-button' value = 'Send'/>
